7f13e15d73a192f3c2a6d39487ff3f5c459701fd
goeunleee <goeun3501@gmail.com>
2021-02-04 17:49:27 +0900

    1.3 tweet create read

--- src/fBase.js
+++ src/fBase.js
@@ -1,6 +1,6 @@
 import firebase from "firebase/app";
 import "firebase/auth";
-
+import "firebase/firestore";
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_Auth_Domain,
@@ -12,3 +12,4 @@ const firebaseConfig = {
  firebase.initializeApp(firebaseConfig);
  export const firebaseInstance = firebase;
  export const authService = firebase.auth();
+ export const dbService = firebase.firestore();
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,50 @@
-import React from "react";
+import { dbService } from "fBase";
+import React, { useEffect, useState } from "react";
 
-const Home = ()=> <span>Home</span>
+const Home = ()=> {
+    const [tweet, setTweet]=useState("");
+    const [tweets,setTweets]=useState([]);
+    const getTweets = async()=>{
+        const dbTweets = await dbService.collection("tweets").get();
+        dbTweets.forEach(document=>{
+            const tweetObject = {
+                ...document.data(),
+                id:document.id,
+            };
+            setTweets(prev =>[document.data(), ...prev]);
+        })
+    }
+    useEffect(()=>{
+       getTweets();
+    }, [])
+    const onSubmit = async(event) =>{
+        event.preventDefault();
+        await dbService.collection("tweets").add({
+            tweet,
+            createdAt:Date.now()
+        });
+        setTweet("");
+   
+    }
+    const onChange = (event) =>{
+        const{
+            target:{value}
+        }= event;
+        setTweet(value);
+    }
+    return(
+ <div>
+    <form onSubmit={onSubmit}>
+        <input value={tweet} onChange={onChange} type="text" placeholder="what's on your mind?" maxLength={120}/>
+        <input type="submit" value="tweet"/>
+    </form>
+    <div>
+        {tweets.map(tweet => 
+        <div key="id">
+            <h4>{tweet.tweet}</h4>
+        </div>)}
+    </div>
+</div> 
+ );
+}
 export default Home;
